122d020a70be3a3d77134843eb1d905fb6de6590
quanjingkuan <1171051103@qq.com>
2016-12-22 22:00:18 +0800

    Finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<div class="col-md-12">
+  <footer class="footer">
+    <nav>
+      <ul>
+        <li><a href="/about">关于</a></li>
+      </ul>
+    </nav>
+  </footer>
+</div>
--- resources/views/staticpage/home.blade.php
+++ resources/views/staticpage/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 @section('title','主页')
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron text-center">
+      <h1>Hello,Laravel</h1>
+      <p>
+        你现在看到的是<a href="#" ><h1>水萌信息技术首页</h1></a>
+      </p>
+      <p>
+     一切，将从这里开始。
+   </p>
+   <p>
+     <a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">现在注册</a>
+   </p>
+  </div>
 @endsection
