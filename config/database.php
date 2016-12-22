9fcb73dbcfe668ccfe578b1b80b4316c0ff8e28c
quanjingkuan <1171051103@qq.com>
2016-12-22 23:29:05 +0800

    Updated database configuration

--- config/database.php
+++ config/database.php
@@ -125,3 +125,70 @@ return [
     ],
 
 ];
+$db_config = get_db_config();
+
+return [
+
+    'fetch' => PDO::FETCH_CLASS,
+
+    'default' => $db_config['connetion'],
+
+    'connections' => [
+
+        'sqlite' => [
+            'driver'   => 'sqlite',
+            'database' => database_path('database.sqlite'),
+            'prefix'   => '',
+        ],
+
+        'mysql' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
+        'pgsql' => [
+            'driver'   => 'pgsql',
+            'host'     => $db_config['host'],
+            'database' => $db_config['database'],
+            'username' => $db_config['username'],
+            'password' => $db_config['password'],
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+        ],
+
+        'sqlsrv' => [
+            'driver'   => 'sqlsrv',
+            'host'     => env('DB_HOST', 'localhost'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset'  => 'utf8',
+            'prefix'   => '',
+        ],
+
+    ],
+
+    'migrations' => 'migrations',
+
+    'redis' => [
+
+        'cluster' => false,
+
+        'default' => [
+            'host'     => env('REDIS_HOST', 'localhost'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port'     => env('REDIS_PORT', 6379),
+            'database' => 0,
+        ],
+
+    ],
+
+];
